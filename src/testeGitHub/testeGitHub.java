2183e23309444e3f79379554a39e4b203961226d
danieldruszcz <danieldruszcz@hotmail.com>
2015-08-28 17:27:02 -0300

    meu primeiro commit

---
+++ src/testeGitHub/testeGitHub.java
@@ -0,0 +1,29 @@
+package testeGitHub;
+
+public class testeGitHub {
+	private String nome;
+	private String CPF;
+	private String dataNascimento;
+	private String getNome() {
+		return nome;
+	}
+	private void setNome(String nome) {
+		this.nome = nome;
+	}
+	private String getCPF() {
+		return CPF;
+	}
+	private void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	private String getDataNascimento() {
+		return dataNascimento;
+	}
+	private void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
+	
+
+}
